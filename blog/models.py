8861ae7e2ffa94dd99d3ae705acd322cd8e5ab07
Declan <laresearchlabs@gmail.com>
2024-07-13 08:19:09 +0100

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,27 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excert = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"The title of the post is: {self.title}| written by {self.author}"
+
+    class Meta:
+        ordering = ["created_on"]
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+    
+    class Meta:
+        ordering =['created_on']
